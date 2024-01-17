cefa249caa63f711817a7c19d4f41e1303e18e8a
Alef Ligiero <alefligieroagb@gmail.com>
2024-01-17 18:30:05 -0300

    Projeto criado

---
+++ src/test/java/com/alefligiero/agenciabackend/AgenciaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alefligiero.agenciabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
